e5be04b920b3ae0b2307f911409a7f1fec45c0b4
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-05 09:48:30 -0600

    chore: actualizar manejo de archivos en la carga

--- app.py
+++ app.py
@@ -49,6 +49,55 @@ def upload_file():
 
     return jsonify(message='Archivos subidos exitosamente', files=uploaded_files), 200
 
+@app.route('/upload-signature', methods=['POST'])
+def upload_signature():
+    if 'file' not in request.files or 'nss' not in request.form:
+        return jsonify(message='Por favor proporciona un archivo y un NSS'), 400
+
+    nss = request.form['nss']
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify(message='Por favor sube un archivo'), 400
+
+    # Crear directorio para el NSS si no existe
+    nss_dir = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(nss))
+    if not os.path.exists(nss_dir):
+        os.makedirs(nss_dir)
+
+    # Crear subcarpeta 'autorización' dentro del directorio NSS si no existe
+    authorization_dir = os.path.join(nss_dir, 'autorización')
+    if not os.path.exists(authorization_dir):
+        os.makedirs(authorization_dir)
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(authorization_dir, filename)
+
+    # Verificar si el archivo ya existe y agregar sufijo numérico si es necesario
+    if os.path.exists(file_path):
+        base, extension = os.path.splitext(filename)
+        counter = 1
+        while os.path.exists(file_path):
+            new_filename = f"{base}_v{counter}{extension}"
+            file_path = os.path.join(authorization_dir, new_filename)
+            counter += 1
+        filename = new_filename
+
+    file.save(file_path)
+
+    return jsonify(message='Archivo de firma subido exitosamente', file=filename), 200
+
+@app.route('/check-signature/<nss>', methods=['GET'])
+def check_signature(nss):
+    # Ruta de la carpeta de autorización del NSS
+    authorization_dir = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(nss), 'autorización')
+
+    # Verificar si la carpeta de autorización existe y contiene archivos
+    if os.path.exists(authorization_dir) and os.listdir(authorization_dir):
+        return jsonify(signature_exists=True), 200
+    else:
+        return jsonify(signature_exists=False), 200
+
 @app.route('/files/<nss>', methods=['GET'])
 def list_files(nss):
     # Ruta de la carpeta del NSS
